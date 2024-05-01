15a5480525b33a80bd8fd7fd8cdbdc0994ce2f84
Soumik Mukhopadhyay <soumikmukhopadhyay45@gmail.com>
2024-05-01 22:46:17 +0530

    login page backend work wip

---
+++ resources/views/layout/admin_login.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link href="{{ asset('public/admin_login/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/admin_login/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/admin_login/css/style.css') }}" rel="stylesheet">
+
+    <title>Educational Bootstrap 5 Login Page Website Tampalte</title>
+  </head>
+  <body>
+  @yield('content');
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/login', [AdminController::class, 'login']);
 });
